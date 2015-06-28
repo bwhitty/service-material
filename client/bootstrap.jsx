9a96fdce2fbdcbf6d8b9959e3fc1048892abc7ab
Braden Whitten <bwhitten518@gmail.com>
2015-06-28 11:28:38 -0400

    Actaully enable routes, todo move these to routes.jsx

--- client/bootstrap.jsx
+++ client/bootstrap.jsx
@@ -33,7 +33,16 @@ if (Meteor.isClient) {
     React.render((
       <Router history={new HashHistory()}>
         <Route path="/" component={UiApp}>
-          <Route path="sign-in" component={UiSignIn}></Route>
+          <Route path="sign-in" component={UiSignIn} />
+          <Route path="marketplace" component={UiMarketplace}>
+            <Route path="/" component={UiMarketplaceLanding} />
+            <Route path="request" component={UiMarketplaceRequest}>
+
+            </Route>
+            <Route path="search" component={UiMarketplaceSearch}>
+
+            </Route>
+          </Route>
         </Route>
       </Router>
     ), document.getElementById("container"));
